1e734f361934a097ea6e59b16fc75039ca3917ee
Thanhtien96 <thanhtien2094@gmail.com>
2024-06-09 21:23:48 +0700

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-around">
+        <h1 className="border border-solid border-transparent hover:border-cyan-500 transition-all duration-700">Admin Template</h1>
+        <h1 className="bg-red-900 text-white w-1/2">Admin Template</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
